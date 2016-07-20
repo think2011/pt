1739ace7f5dd9fa10b5d35c2ac1af5b3e1d52a8c
think2011 <452125301@qq.com>
2016-07-20 18:33:52 +0800

    fix sort

--- src/editors/goods.vue
+++ src/editors/goods.vue
@@ -33,12 +33,16 @@
                                     class="btn-sm"
                                     color="default"
                                     type="flat"
+                                    v-do-order.up
+                                    :do-order-options="{items:componentValue.value,index:$index}"
                                     icon="expand_less">
                             </ui-icon-button>
                             <ui-icon-button
                                     class="btn-sm"
                                     color="default"
                                     type="flat"
+                                    v-do-order.down
+                                    :do-order-options="{items:componentValue.value,index:$index}"
                                     icon="expand_more">
                             </ui-icon-button>
                             <ui-icon-button
@@ -53,7 +57,7 @@
                 </ul>
             </ui-tab>
 
-            <ui-tab header="编辑内容">
+            <ui-tab header="属性">
                 <ul class="goods-items property">
                     <li
                             class="item"
@@ -222,18 +226,11 @@
     import tabs from '../components/tabs.vue'
     import tab from '../components/tab.vue'
     import selectGoods from '../components/select-goods.vue'
-    import {showToast} from '../vuex/actions'
 
     export default{
         props: {
             componentValue: {
                 type: Object
-            }
-        },
-
-        vuex: {
-            actions: {
-                showToast
             }
         },
 
@@ -246,10 +243,7 @@
         methods: {
             del (item) {
                 if (this.componentValue.value.length <= this.componentValue.options.minLen) {
-                    return this.showToast({
-                        message : `至少要留${this.componentValue.options.minLen}个宝贝`,
-                        duration: 3000
-                    })
+                    return alert(`至少要留${this.componentValue.options.minLen}个宝贝`)
                 }
 
                 this.componentValue.value.$remove(item)
